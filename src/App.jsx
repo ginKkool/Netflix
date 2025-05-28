ca64d9f612bf809f3855c5c1387b6d18040a5ee2
ginKkool <ginkko71@gmail.com>
2025-05-28 14:34:17 +0200

    base 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import "./App.css";
-import Section from "./components/section";
+import data from "./assets/data-movies.json";
+import Logo from "./assets/logo-netF.png";
 
 const App = () => {
   return (
-    <main>
-      <header className="wrapper">
-        <img src="./public/logo-netF.png" alt="" />
-      </header>
-      <div className="wrapper">
-        <Section
-          category="Emissions TV"
-          images="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABeC6G0iyqHxQH1Uz7udc5R_J_7kQTm_8L59OXFsF0K_h00ynxeYSDovahSubumX6FPyJnvhT5-xKmF6QUHuiUH6Y0pX11jPD1NfgMLDCiEIoMDELbL5EE46peUlo_hpwacw.jpg"
-        />
-      </div>
-    </main>
+    <>
+      <body>
+        <header className="wrapper">
+          <img src={logo} alt="logo Netflix" />
+        </header>
+
+        <section className="wrapper">
+          <h1>test</h1>
+        </section>
+      </body>
+    </>
   );
 };
 
